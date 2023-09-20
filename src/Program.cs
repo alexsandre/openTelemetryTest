206ec15874e99a7fd2868eca4886988255a42010
Alexsandre Almeida <alexsandrer@gmail.com>
2023-09-19 22:27:02 -0300

    Configure asp.net core metrics and Prometheus exporter

--- src/Program.cs
+++ src/Program.cs
@@ -1,18 +1,30 @@
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 using OpenTelemetryTest.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(o => o.UseInMemoryDatabase("db"));
-
 builder.Services.AddScoped<AppDbContext>();
+
+var tracingOtlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
+var otel = builder.Services.AddOpenTelemetry();
+
+otel.ConfigureResource(resource => resource.AddService(serviceName: builder.Environment.ApplicationName));
+
+otel.WithMetrics(metrics => 
+    metrics
+    .AddAspNetCoreInstrumentation()
+    .AddMeter("Microsoft.AspNetCore.Hosting")
+    .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+    .AddPrometheusExporter());
 
 var app = builder.Build();
 
@@ -26,5 +38,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
